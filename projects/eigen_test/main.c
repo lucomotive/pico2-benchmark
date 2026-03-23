2251dd866287f510504af524a03cbc8809ba9d88
lucomotive <lucomotive@nixos.com>
2026-03-23 09:29:14 +0100

    rename eigen to eigen_test

---
+++ projects/eigen_test/main.c
@@ -0,0 +1,15 @@
+
+#include "pico/stdlib.h"
+#include <Eigen/Dense>
+
+#define REAL float
+
+Eigen::Matrix<REAL, 10, 10> mat;
+
+int main() {
+  sleep_ms(5000); // Warten auf Serial-Verbindung
+
+  println("Initialisiere Matrizen mit Random-Werten...\n");
+  mat.setRandom();
+  std::cout << "A = " << A << std::endl;
+}
