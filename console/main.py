dc6d9bcd07cc96a5996639afcb9bf10639ca4eca
lucomotive <lucomotive@nixos.com>
2026-05-05 17:55:41 +0200

    new python script reads file instead of command line. also files for each benchmark

--- console/main.py
+++ console/main.py
@@ -9,123 +9,73 @@
 import asyncio
 import serial
 import json
-from prompt_toolkit import PromptSession
-from prompt_toolkit.completion import WordCompleter
-from prompt_toolkit.formatted_text import HTML
-from prompt_toolkit.patch_stdout import patch_stdout
-from prompt_toolkit.history import FileHistory
-from prompt_toolkit import prompt
-
+import sys
+import argparse
 
 
 ser = serial.Serial('/dev/ttyACM0', baudrate=115200, timeout=1)
 
 
-def receive_results(name, precision):
-    #with open(f"results/{name}-{precision}-test-algorihtm.csv", "w") as f:
+def receive_results():
+    file = None
+
     while True:
-        line = ser.readline().decode().strip()
-        match line.split(" "):
-            case ["SOF", *args]:
-                print("Starting writing results to file...")
+        line = ser.readline()
+        decoded = line.decode().strip()
+        if not decoded:
+            continue
+        match decoded.split(" "):
+            case ["SOF", type, name, precision]:
+                if file:
+                    print("Received start of file signal, but file already open")
+                    continue
+                print("Received start of file signal. Opening file...")
+                file = open(f"results/{name.upper()}-{precision.upper()}-{type.upper()}.csv", "w")
+            case ["EOF", *_]:
+                if not file:
+                    print("Received end of file signal, but no file open")
+                    continue
+                print("Received end of file signal. Closing file...")
+                file.close()
+                file = None
+            case ["EOT"]:
+                # end of transmission
+                if file:
+                    file.close()
                 break
-            case ["ERROR", *args]:
-                print(line)
-                return
+            case ["Out", "of", "memory"]:
+                print(decoded)
+                print("Please reboot pico")
+                break
             case anything if anything:
-                print(line)
-                #f.write(line + "\n")
-
-    with open(f"results/{name}-{precision}.csv", "w") as f:
-        # begin writing
-        while True:
-            line = ser.readline().decode().strip()
-            match line:
-                case "EOF":
-                    print("Benchmark finished")
-                    break
-                case "Out of memory":
-                    print(line)
-                    print("Please reboot pico")
-                    break
-                case anything if anything:
-                    f.write(line + "\n")
-                    f.flush()
+                print(decoded)
+                if file:
+                    file.write(decoded + "\n")
+                    file.flush()
+    # close file after loop
+    if file:
+        file.close()
 
 
 
 
 
+def main():
+    parser = argparse.ArgumentParser(prog='console')
+    parser.add_argument('--file', help='file in json format specifying the benchmark parameters', type=str, required=True)
+    args = parser.parse_args(sys.argv[1:])
 
-def help():
-    print(f'''Usage: run --benchmark=<qr|lu|matmul|...> --precision=<float|double>
 
-'exit' or ctrl-c to quit application''')
+    file = open(args.file, "r")
+    content = file.read()
+    file.close()
 
-async def main():
-    session = PromptSession(
-        completer=WordCompleter(['exit', 'help', 'run']),
-        history=FileHistory('.console_history'),
-    )
+    # check for valid json. also force json to be a one-liner
+    j = json.loads(content)
+    message = json.dumps(j)
+    print(f"Sending to pico...\n{content}")
+    ser.write(f"{message}\n".encode())
+    receive_results()
 
-    # patch_stdout stops incoming pico lines from breaking the input prompt
-    with patch_stdout():
-        while True:
-            try:
-                cmd = await session.prompt_async(
-                    HTML("<ansigreen>pico</ansigreen> <b>></b> ")
-                )
-                parts = cmd.strip().split(" ")
-                if not parts:
-                     continue
 
-                match parts:
-                    case ['run', *args]:
-                        allowedArgs = [
-                            "benchmark",
-                            "precision",
-                            "iterations",
-                            "min-dimension",
-                            "max-dimension"
-                        ]
-                        requiredArgs = [
-                            "benchmark"
-                        ]
-                        defaultArgs = {
-                            "precision": "float",
-                            "min-dimension": 5,
-                            "max-dimension": 90,
-                            "iterations": 1000
-                        }
-
-                        # merge defaultArgs with provided args with | (duplicates prioritise right side)
-                        argsDict = defaultArgs | dict((arg.lstrip("-").split("=", 1)) for arg in args)
-
-                        # parse to int
-                        argsDict["iterations"] = int(argsDict["iterations"])
-                        argsDict["min-dimension"] = int(argsDict["min-dimension"])
-                        argsDict["max-dimension"] = int(argsDict["max-dimension"])
-
-                        missing = [item for item in requiredArgs if item not in argsDict]
-                        if missing:
-                            print(f"The following args are missing: {missing}")
-                            continue
-                        invalid = [item for item in argsDict if item not in allowedArgs]
-                        if invalid:
-                            print(f"The following args are invaild: {invalid}")
-                            continue
-
-                        message = json.dumps(argsDict) + "\n"
-                        ser.write(message.encode())
-                        receive_results(name=argsDict["benchmark"], precision=argsDict["precision"])
-
-                    case ['exit']:
-                        print("Exiting...")
-                        break
-                    case _:
-                        help()
-            except (EOFError, KeyboardInterrupt):
-                print("Exiting...")
-                break
-
-asyncio.run(main())
+main()
