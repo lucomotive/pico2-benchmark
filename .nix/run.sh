4c9f01397e24672a600c38aeff52b2a967b8188c
lucomotive <lucomotive@nixos.com>
2026-05-11 17:40:20 +0200

    remove time stamp from run.sh

--- .nix/run.sh
+++ .nix/run.sh
@@ -2,8 +2,6 @@
 set -e
 
 BUILD_DIR="build"
-# tracks last successful flash
-STAMP="$BUILD_DIR/.flash_stamp"
 
 # build process
 cmake -B "$BUILD_DIR" -S .
@@ -16,14 +14,5 @@ if [ -z "$UF2" ]; then
     exit 1
 fi
 
-# compare stamp with .uf2 file
-# flash only if .uf2 is newer that stamp
-if [ "$UF2" -nt "$STAMP" ]; then
-    echo "Flashing $UF2..."
-    picotool load "$UF2" --force --execute
-    touch "$STAMP"
-    echo "Waiting for pico to start..."
-    sleep 3
-else
-    echo "Build unchanged, skipping flash..."
-fi
+echo "Flashing $UF2..."
+picotool load "$UF2" --force --execute
