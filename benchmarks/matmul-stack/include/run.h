cfc5a60980fc5a018fd7d413fb2891e02fa1297b
lucomotive <lucomotive@nixos.com>
2026-05-20 22:10:53 +0200

    matmul stack

--- benchmarks/matmul-stack/include/run.h
+++ benchmarks/matmul-stack/include/run.h
@@ -1,43 +1,26 @@
 #pragma once
 
 #include "benchmarks/benchmarks.h"
-#include "print.h"
-#include "timer.h"
+#include "utils.h"
 #include <cstdint>
 #include <cstdio>
 
 using namespace benchmarks;
 
-template <typename P> inline void op(uint32_t x, uint32_t y, uint32_t z) {
-  Mat<P> M1(Mat<P>::Random(x, y));
-  Mat<P> M2(Mat<P>::Random(y, z));
-  Mat<P> RES;
-  auto time = matmul::matmul(M1, M2, RES);
-  printf("%u,%u,%u,%llu\n", y, x, z, time);
+template <typename P, uint32_t x, uint32_t y, uint32_t z> inline void op() {
+  Matrix<P, x, y> M1(Matrix<P, x, y>::Random());
+  Matrix<P, y, z> M2(Matrix<P, y, z>::Random());
+  Matrix<P, x, z> RES;
+  auto time = matmul::stack<P>(M1, M2, RES);
+  printf("%u,%u,%u,%e\n", y, x, z, time.count());
 };
 
 template <typename P> void run() {
   // run benchmark
-  printf("x,y,z,time_us\n");
-  const uint16_t min = 5;
-  const uint16_t max = 500;
-  const uint16_t step = 2;
-  const float max_ratio = 0.35;
-  for (uint16_t x = min; x <= max; x += (uint16_t)step) {
-    for (uint16_t y = min; y <= x; y += (uint16_t)step) {
-      for (uint16_t z = min; z <= y; z += (uint16_t)step) {
-        if ((float)y < (float)x * max_ratio ||
-            (float)z < (float)y * max_ratio || (float)z < (float)x * max_ratio)
-          continue;
+  printf("x,y,z,time_s\n");
+  constexpr uint16_t min = 5;
+  constexpr uint16_t max = 100;
+  constexpr uint16_t step = 2;
 
-        op<P>(x, y, z);
-        if (x != y)
-          op<P>(y, x, z);
-        if (y != z)
-          op<P>(x, z, y);
-        if (z != x)
-          op<P>(z, y, x);
-      }
-    }
-  }
+  repeat<max, step, min>([&](auto x) { op<P, x, x, x>(); });
 }
