0cdd97a41d1bf10ac11c41077b388558ef973ff3
lucomotive <lucomotive@nixos.com>
2026-05-20 17:40:28 +0200

    nix shell scripts

--- .nix/pico/flash.sh
+++ .nix/pico/flash.sh
@@ -4,13 +4,14 @@ set -e
 BUILD_DIR="build"
 TARGET="pico"
 PROJECT=$1
+PRECISION=$2
 
 # build process
 cmake -B "$TARGET/$BUILD_DIR" -S "$TARGET" -DCMAKE_BUILD_TYPE=Release
-cmake --build "$TARGET/$BUILD_DIR" -j$(nproc) --target "$PROJECT"
+cmake --build "$TARGET/$BUILD_DIR" -j$(nproc) --target "$PROJECT-$PRECISION"
 
 # find uf2 file
-UF2=$(ls "$TARGET/$BUILD_DIR/$PROJECT/"*.uf2 2>/dev/null | head -1)
+UF2=$(ls "$TARGET/$BUILD_DIR/$PROJECT/$PROJECT-$PRECISION".uf2 2>/dev/null | head -1)
 if [ -z "$UF2" ]; then
     echo "No .uf2 file found"
     exit 1
@@ -20,5 +21,6 @@ echo "Flashing $UF2..."
 picotool load "$UF2" --force --execute --verify
 
 sleep 3
+echo "Waiting for pico to start..."
 
-pico-run $PROJECT
+pico-run $PROJECT $PRECISION
--- .nix/pico/run.sh
+++ .nix/pico/run.sh
@@ -3,5 +3,6 @@ set -e
 
 TARGET="pico"
 PROJECT=$1
+PRECISION=$2
 
-cat /dev/ttyACM0 | tee "$TARGET/results/$PROJECT".csv
+cat /dev/ttyACM0 | tee "$TARGET/results/$PROJECT-$PRECISION".csv
