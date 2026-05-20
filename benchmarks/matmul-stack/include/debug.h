aec09f34d192bea6a949fcf7a94dbe68188f74bc
lucomotive <lucomotive@nixos.com>
2026-05-20 22:10:16 +0200

    matmul stack

--- benchmarks/matmul-stack/include/debug.h
+++ benchmarks/matmul-stack/include/debug.h
@@ -7,12 +7,12 @@
 
 using namespace benchmarks;
 
-template <typename P> void debug(uint32_t x, uint32_t y, uint32_t z) {
-  Mat<P> M1(Mat<P>::Random(x, y));
-  Mat<P> M2(Mat<P>::Random(y, z));
-  Mat<P> RES;
-  auto time = matmul::matmul(M1, M2, RES);
+template <typename P, uint32_t x, uint32_t y, uint32_t z> void debug() {
+  Matrix<P, x, y> M1(Matrix<P, x, y>::Random());
+  Matrix<P, y, z> M2(Matrix<P, y, z>::Random());
+  Matrix<P, x, z> RES;
+  auto time = matmul::stack<P>(M1, M2, RES);
 
   print_all<'\n'>("M1:", M1.format(EIGEN_FMT), "M2:", M2.format(EIGEN_FMT),
-                  "RES:", RES.format(EIGEN_FMT), "TIME:", time);
+                  "RES:", RES.format(EIGEN_FMT), "TIME:", time.count());
 }
