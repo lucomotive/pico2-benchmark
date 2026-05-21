b3c599235642705ad58c521e3895f3df0c1fd18f
lucomotive <lucomotive@nixos.com>
2026-05-21 19:00:00 +0200

    fixed some bugs in scripts

--- .bash/run.sh
+++ .bash/run.sh
@@ -8,5 +8,5 @@ PRECISION=$3
 if [ $TARGET -eq "pico" || $TARGET -eq "pico2" ]; then
     cat /dev/ttyACM0 | tee "results/$TARGET/$PROJECT-$PRECISION".csv
 elif [ $TARGET -eq "host" ]; then
-
+    ./$BUILD_DIR/benchmarks/$PROJECT/$PROJECT-$PRECISION | tee "results/$TARGET/$PROJECT-$PRECISION".csv
 fi
